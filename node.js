71bb9798c6f543b9ec1b28a73faf91795d680020
Aero Wang <aero.windwalker@gmail.com>
2015-01-14 17:40:28 -0500

    v2.0.0

--- node.js
+++ node.js
@@ -11,43 +11,60 @@ console.log('Server running at http://127.0.0.1:1337/');
 var proxyChecker = require('proxy-checker');
 var fs = require('fs');
 
+function nth_occurrence (string, char, nth) {
+    var first_index = string.indexOf(char);
+    var length_up_to_first_index = first_index + 1;
+
+    if (nth == 1) {
+        return first_index;
+    } else {
+        var string_after_first_occurrence = string.slice(length_up_to_first_index);
+        var next_occurrence = nth_occurrence(string_after_first_occurrence, char, nth - 1);
+
+        if (next_occurrence === -1) {
+            return -1;
+        } else {
+            return length_up_to_first_index + next_occurrence;  
+        }
+    }
+}
+
 function get_line(filename, line_no, callback) {
 	fs.readFile(filename, function (err, data) {
 		if (err) throw err;
-
 		// Data is a buffer that we need to convert to a string
 		// Improvement: loop over the buffer and stop when the line is reached
-		var lines = data.toString('utf-8').split("\n");
-		var firstLineBreak = data.toString('utf-8').indexOf("\n");
 		var originalText = data.toString('utf-8');
-		var newText = originalText.substr(firstLineBreak + 1);
+		var lines = originalText.split("\n");
+		var lineBreak = nth_occurrence (originalText, "\n", proxyNum);
+		var newText = originalText.substr(lineBreak + 1);
 		if(+line_no > lines.length){
 			return callback('File end reached without finding line', null);
 		}
 
-		callback(null, lines[+line_no], newText);
+		callback(null, lines[+line_no], line_no, newText);
 	});
 }
 
 var operationsCompleted = 0;
 function operation() {
     ++operationsCompleted;
-    if (operationsCompleted === proxyNum * 2) after_forloop(); 
+    if (operationsCompleted === proxyNum) after_forloop(); 
 }
+
 for (i = 0; i < proxyNum; i++) {
-	get_line('proxy.txt', i, function(err, line, newText){
+	get_line('proxy.txt', i, function(err, line, line_no, newText){
 		fs.appendFile('cache.txt', line + '\n', function (err) {
-			if (err) throw err;
-			operation();		
-		});		
-		fs.writeFile('proxy.txt', newText, function (err) {
 			if (err) throw err;
 			operation();
 		});
-	})
-	if (i === proxyNum - 1) {
-		console.log("cache.txt is ready.");
-	}
+		if (line_no === proxyNum - 1) {
+			fs.writeFile('proxy.txt', newText, function (err) {
+				if (err) throw err;					
+			});
+			console.log("Proxy test began.");
+		}
+	});
 }
 
 function after_forloop() {
@@ -56,7 +73,7 @@ function after_forloop() {
 		'cache.txt',
 		{
 			// the complete URL to check the proxy
-			url: targetURL,
+			url: targetURL,	
 			// an optional regex to check for the presence of some text on the page
 			regex: /.*/
 		},
